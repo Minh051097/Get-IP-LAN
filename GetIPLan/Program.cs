3008a7765ebe667b2e522fdc967f0020f7d3809b
AsusTp500 <AsusTp500@ASUS>
2018-03-09 14:35:40 +0700

    Add project files.

---
+++ GetIPLan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace GetIPLan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strHostIP = Dns.GetHostName();
+            Console.WriteLine("Host IP: "+ strHostIP);
+
+            IPHostEntry iphentry = Dns.GetHostByName(strHostIP);
+
+            int Ips = 0;
+            foreach (IPAddress ipaddress in iphentry.AddressList)
+            {
+                Console.WriteLine("IP#" + ++Ips + ":" + ipaddress.ToString());
+            }
+
+        }
+    }
+}
